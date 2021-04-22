730aa768f671562ceb6be7eb9cf0dbf8aa52e020
Chris Klemz <crklemz@gmail.com>
2021-04-21 19:59:42 -0500

    getting set up, labeled some things, started a little pseudocode

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,21 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = []
+
+function addItem {
+  //Take an input parameter for a string 'item'
+  //add the new item to the global array 'basket'
+  //return true to verify the item was added
+}
+
+function listItems {
+  //loop through the 'basket' array and
+  //log each item on a new line in the console
+
+}
+
+function empty {
+  //reset the 'basket' array so it is empty
+}
