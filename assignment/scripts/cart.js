1ee9d1372c93bcd7ed7c0f75f5aadb422210724e
Chris Klemz <crklemz@gmail.com>
2021-04-25 16:21:54 -0500

    assignment complete

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,20 +2,63 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
-let basket = []
+const basket = [];
+const maxItems = 5;
 
-function addItem {
-  //Take an input parameter for a string 'item'
-  //add the new item to the global array 'basket'
-  //return true to verify the item was added
+function addItem ( item ){
+  if (item) {
+  basket.push(item);
+  console.log('Item added was', item);
+  return true;
+  }// End if statement
+else {
+  console.log('no item was added to the list');
+  return false;
+}//end else statement
+}//End addItem
+
+function listItems () {
+  for ( i=0; i < basket.length; i++ ) {
+    console.log(`item number`, i+1 , `is ${basket[i]}`);
+  }
+}// End listItems
+
+function empty () {
+  basket.length = 0
+  if (basket.leangth > 0) {
+    console.log(`basket is ${basket}`);
+  }
+  else {
+    console.log('Basket is empty');
+  }
+  return;
+}//End empty
+
+function isFull () {
+  if ( basket.length >= maxItems ) {
+    console.log('basket is full');
+    return true;
+  }
+  else {
+    console.log('basket is not full');
+    return false;
+  }
+}//End isFull
+
+if (basket.leangth > 0) {
+  console.log(`basket is ${basket}`);
 }
-
-function listItems {
-  //loop through the 'basket' array and
-  //log each item on a new line in the console
-
+else {
+  console.log('Basket is empty');
 }
+addItem ();
+addItem( 'Bacon' );
+empty ();
+addItem ('squid');
+console.log(`Basket is ${basket}`);
 
-function empty {
-  //reset the 'basket' array so it is empty
-}
+addItem('Pizza');
+addItem('Cheese');
+
+listItems();
+isFull();
